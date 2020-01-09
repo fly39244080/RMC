ea3cbe1ead3837dc9a903bbf8d3d960d50c646f5
fly39244080 <fly39244080@163.com>
2020-01-09 11:54:26 +0800

    fix

--- src/assets/js/net.js
+++ src/assets/js/net.js
@@ -1,6 +1,9 @@
-// var reportImg = require('../images/sign/report.png');
 import reportImg from '../images/sign/report.png';
 import 'assets/styles/net.less';
 var $netReport = $('#netReport');
 
 $netReport.find('img').attr('src',reportImg);
+
+$('#btnTryLesson').on('click',function(){
+    location.href = '/audition.html';
+})
